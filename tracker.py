c3f184c5e2cf17f80ef5a3e0d5592397fbd47fd7
tbhimtbh <aalex.6r.b1@gmail.com>
2023-05-04 16:56:36 +0800

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,19 +1,32 @@
 import requests
+import hashlib
 import time
 
-def get_fb_page(profile_id):
-    url = f"https://www.facebook.com/profile.php?id={profile_id}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
-    response = requests.get(url, headers=headers)
-    return response.text
+# Prompt user for Facebook profile ID
+profile_id = input("Enter the Facebook profile ID to monitor: ")
 
-profile_id = input("Enter the profile ID you want to check: ")
-previous_data = get_fb_page(profile_id)
+# Construct the URL to monitor
+url = f"https://www.facebook.com/profile.php?id={profile_id}"
 
+# Use a hash of the page content to detect changes
+hash_old = ""
+
+# Continuously scrape the page
 while True:
-    current_data = get_fb_page(profile_id)
-    if current_data != previous_data:
-        print("ALERT: A new post has been added!")
-        previous_data = current_data
-    time.sleep(60)
+    # Make a request to the URL
+    response = requests.get(url)
+
+    # Compute the hash of the response content
+    hash_new = hashlib.sha256(response.content).hexdigest()
+
+    # If the hash has changed, alert the user
+    if hash_new != hash_old:
+        print("ALERT: The page has changed!")
+        hash_old = hash_new
+
+    # If the hash has not changed, indicate that the program is still running
+    else:
+        print("Scraping... Tracking...")
+    
+    # Wait for 5 seconds before scraping again
+    time.sleep(5)
