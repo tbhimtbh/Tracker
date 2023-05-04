44ea0578d7a2cb152d511dadc81c3d7273570bbd
tbhimtbh <aalex.6r.b1@gmail.com>
2023-05-04 16:25:08 +0800

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -26,10 +26,13 @@ def get_user_input():
         ACCESS_TOKEN = input('Enter your ACCESS_TOKEN: ')
 
     # validate USER_ID and ACCESS_TOKEN
-    user_url = f'{FB_API_URL}{USER_ID}?access_token={ACCESS_TOKEN}'
+    user_url = f'{FB_API_URL}{USER_ID}?access_token={ACCESS_TOKEN}&fields=name'
     try:
         response = requests.get(user_url)
         response.raise_for_status()
+        data = response.json()
+        user_name = data['name']
+        print(f'Successfully authenticated as {user_name} ({USER_ID})')
     except requests.exceptions.HTTPError as e:
         print(f'Error validating USER_ID and ACCESS_TOKEN: {e}')
         USER_ID = ''
@@ -42,35 +45,37 @@ def monitor_user_profile():
     """
     global USER_ID, ACCESS_TOKEN
 
-    url = f'{FB_API_URL}{USER_ID}?fields=updated_time&access_token={ACCESS_TOKEN}'
     last_updated_time = None
     last_post_time = None
 
     while True:
         try:
             # check profile update
-            response = requests.get(url)
+            profile_url = f'{FB_API_URL}{USER_ID}?access_token={ACCESS_TOKEN}&fields=updated_time'
+            response = requests.get(profile_url)
             response.raise_for_status()
             data = response.json()
             updated_time_str = data['updated_time']
             updated_time = time.strptime(updated_time_str, '%Y-%m-%dT%H:%M:%S+0000')
 
             if last_updated_time is None or updated_time > last_updated_time:
-                message = f'The user updated their profile at {updated_time_str}'
+                message = f'{USER_ID} updated their profile at {updated_time_str}'
                 send_notification(message)
                 last_updated_time = updated_time
 
             # check for new post
-            post_url = f'{FB_API_URL}{USER_ID}/feed?access_token={ACCESS_TOKEN}'
+            post_url = f'{FB_API_URL}{USER_ID}/posts?access_token={ACCESS_TOKEN}&fields=created_time'
             post_response = requests.get(post_url)
             post_response.raise_for_status()
             post_data = post_response.json()
-            new_post_time = time.strptime(post_data['data'][0]['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
 
-            if last_post_time is None or new_post_time > last_post_time:
-                message = f'The user posted something new at {post_data["data"][0]["created_time"]}'
-                send_notification(message)
-                last_post_time = new_post_time
+            if 'data' in post_data and len(post_data['data']) > 0:
+                new_post_time = time.strptime(post_data['data'][0]['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
+
+                if last_post_time is None or new_post_time > last_post_time:
+                    message = f'{USER_ID} posted something new at {post_data["data"][0]["created_time"]}'
+                    send_notification(message)
+                    last_post_time = new_post_time
 
         except requests.exceptions.HTTPError as e:
             if e.response.status_code == 190:
